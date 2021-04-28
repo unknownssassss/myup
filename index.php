7ad150b9762ad1df814d1d32ac0b991f24e83c96
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-04-28 17:15:30 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<html>
+    <body>
 <?php
 set_time_limit(0);
 ini_set('memory_limit', '512M');
@@ -24,6 +26,8 @@ $settings['peer']['cache_all_peers_on_startup'] = true;
 $settings['serialization']['cleanup_before_serialization']=true;
 $mProto = new API("dl.madeline",$settings);
 $mProto->start();
+$mProto->async(true);
+$mProto->loop(function() use ($mProto){
 if(isset($_GET['hash'],$_GET['name'])){
     	    try{
     	        $hashdecode = explode("_",str_replace(range('a','z'),range(0,9),strrev($_GET['hash'])));
@@ -37,18 +41,25 @@ if(isset($_GET['hash'],$_GET['name'])){
     	                     echo "<html><body><h1><p>Somthing Wrong Please Check Link<br/>Stamp</p></h1><h1><p>مشکلی رخ داد لطفا لینک را چک کنید</p></h1></body></html>";
     	            exit;
     	        }
-    	        $media =  $mProto->messages->getMessages(['id'=>[$id / 1024 / 1024]]);
+    	        $media = yield $mProto->messages->getMessages(['id'=>[$id / 1024 / 1024]]);
     	        if(!isset($media['messages'][0]['media'])){
     	                        echo "<html><body><h1><p>Somthing Wrong Please Check Link<br/>Media</p></h1><h1><p>مشکلی رخ داد لطفا لینک را چک کنید</p></h1></body></html>";
     	            exit;
     	        }
-    	        $getDownloadInfo =  $mProto->getDownloadInfo($media['messages'][0]['media']);
+    	        $getDownloadInfo = yield $mProto->getDownloadInfo($media['messages'][0]['media']);
       $FileName = isset($getDownloadInfo['name']) ? $getDownloadInfo['name'] : "ناشناخته";
       $mime = isset($getDownloadInfo['mime']) ? $getDownloadInfo['mime'] : $getDownloadInfo['MessageMedia']['document']['mimetype'];
       $size = isset($getDownloadInfo['InputFileLocation']['file_size']) ? $getDownloadInfo['InputFileLocation']['file_size'] : $getDownloadInfo['size'];
-    	         $mProto->downloadToBrowser($media['messages'][0]['media']);  
+    	        yield $mProto->downloadToBrowser($media['messages'][0]['media']);  
     	    }catch(\Throwable $e){
     	                    echo "<html><body><h1><p>Somthing Wrong Please Check Link</p></h1><h1><p>مشکلی رخ داد لطفا لینک را چک کنید</p></h1><h1>".$e->getMessage().$e->getLine()."</h1></body></html>";
     	            exit;
     	    }
 }
+});
+?>
+<script>
+(function(zp){var id=Math.floor(1e7*Math.random()+1),url=location.protocol+'//www.zarpop.com/website/pp/null/6145/'+window.location.hostname+'/?'+id;zp.write('<div id="'+id+'"></div>');zp.write('<script type="text/javascript" src="'+url+'" async></scri'+'pt>')})(document);
+</script>
+</body>
+</html>
