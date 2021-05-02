a1ea511fbc6543400da36cbeb813843a34c3f08b
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-05-02 21:55:18 +0430

    Add files via upload

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ $media = $mProto->messages->getMessages(['id' => [$id / 1024 / 1024]]);
         <!-- footer -->
         <footer class="fixed-bottom bg-light text-center text-lg-start">
             <div class="text-center p-3 txt-footer">
-><a href="https://t.me/file2linkskybot" class="btn btn-secondary">کلیه حقوق برای ربات  file2linkskybot محفوظ است </button>
+><a href="https://t.me/file2linkskybot" class="btn btn-light">کلیه حقوق برای ربات  file2linkskybot محفوظ است </button>
 </div>
             <!-- Copyright -->
         </footer>
