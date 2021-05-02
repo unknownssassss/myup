49b22323e37ebc6f2feabedbcbbb0d60c0f9195f
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-05-02 16:29:22 +0430

    Add files via upload

--- download/index.php
+++ download/index.php
@@ -4,13 +4,10 @@ ini_set('memory_limit', '512M');
 ini_set('max_execution_time', -1);
 date_default_timezone_set("Asia/tehran");
 
-if (!\file_exists('../app/session/madeline.php')) {
-    \copy('https://phar.madelineproto.xyz/madeline.php', '../app/session/madeline.php');
+if (!\file_exists('madeline.php')) {
+    \copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
-echo "<pre>";
-var_dump(scandir('../app/session'));
-exit;
-require('../app/session/madeline.php');
+require('madeline.php');
 use danog\MadelineProto\API;
 use danog\MadelineProto\Logger;
 $settings = [];
@@ -20,7 +17,7 @@ $settings['logger']['logger'] = \danog\MadelineProto\Logger::FILE_LOGGER;
 $settings['logger']['max_size'] = 2 * 1024 * 1024;
 $settings['peer']['cache_all_peers_on_startup'] = true;
 $settings['serialization']['cleanup_before_serialization'] = true;
-$mProto = new API("../app/session/dl.madeline", $settings);
+$mProto = new API("dl.madeline", $settings);
 $mProto->start();
 if (isset($_GET['hash'], $_GET['name'])) {
     try {
