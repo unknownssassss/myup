4f2defcb989836e49d3a0afa6a54bc1e68120516
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-05-04 17:00:22 +0430

    Update index.php

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ $media = $mProto->messages->getMessages(['id' => [$id / 1024 / 1024]]);
             <!-- text -->
             <!-- table -->
             <!-- btn download file-->
-                  <div id="formFooter">
+                  <div id="formContent">
                <a href='<? echo link.base64_decode($_GET["getTheFile"])."/".$FileName.$ext;?>' target="_blank" class="btn btn-secondary btn-lg btn-block">Download - دانلود</a>
                </div>
 </div>
