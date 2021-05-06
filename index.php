b52ca8523d9909ee8855ed8c4582d68961c41383
unknownssassss <mahdi.khaleghi1234@gmail.com>
2021-05-06 12:21:31 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,42 @@
 <?php
-define("link","https://dl.tgdrive.ir/download/");
+define("link", $_SERVER['SERVER_NAME'] ?? $_SERVER['HTTP_HOST']."/download/");
+function toNim($url) {
+
+    $post = ['downloadUri' => str_replace(" ", "", $url)];
+
+    $ch = curl_init('https://www.digitalbam.ir/DirectLinkDownloader/Download');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+    // execute!
+    $response = curl_exec($ch);
+
+    // close the connection, release resources used
+    curl_close($ch);
+    $response = json_decode($response, true);
+    if (isset($response['fileUrl']) && !empty($response['fileUrl'])) {
+        return $response['fileUrl'];
+    }
+    return $url;
+}
 function formatBytes($bytes, $precision = 2) {
 
-        $units = ['B',
-            'KB',
-            'MB',
-            'GB',
-            'TB',
-            'PB',
-            'EB',
-            'ZB',
-            'YB'];
-        $bytes = max($bytes, 0);
-        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-        $pow = min($pow, count($units) - 1);
-        $bytes /= pow(1024, $pow);
+    $units = ['B',
+        'KB',
+        'MB',
+        'GB',
+        'TB',
+        'PB',
+        'EB',
+        'ZB',
+        'YB'];
+    $bytes = max($bytes, 0);
+    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+    $pow = min($pow, count($units) - 1);
+    $bytes /= pow(1024, $pow);
 
-        return round($bytes, $precision) . ' ' . $units[$pow];
-    }
+    return round($bytes, $precision) . ' ' . $units[$pow];
+}
 if (!file_exists("download/index.php")) {
     http_response_code(404);
     exit;
@@ -39,14 +58,14 @@ if ($stamp < time()) {
     exit;
 }
 $media = $mProto->messages->getMessages(['id' => [$id / 1024 / 1024]]);
-        if (!isset($media['messages'][0]['media'])) {
-            http_response_code(404);
-            exit;
-        }
-        $getDownloadInfo = $mProto->getDownloadInfo($media['messages'][0]['media']);
-        $FileName = isset($getDownloadInfo['name']) ? $getDownloadInfo['name'] : "ناشناخته";
-        $ext = isset($getDownloadInfo['ext']) ? $getDownloadInfo['ext'] : $getDownloadInfo['MessageMedia']['document']['ext'];
-        $size = isset($getDownloadInfo['InputFileLocation']['file_size']) ? $getDownloadInfo['InputFileLocation']['file_size'] : $getDownloadInfo['size'];
+if (!isset($media['messages'][0]['media'])) {
+    http_response_code(404);
+    exit;
+}
+$getDownloadInfo = $mProto->getDownloadInfo($media['messages'][0]['media']);
+$FileName = isset($getDownloadInfo['name']) ? $getDownloadInfo['name'] : "ناشناخته";
+$ext = isset($getDownloadInfo['ext']) ? $getDownloadInfo['ext'] : $getDownloadInfo['MessageMedia']['document']['ext'];
+$size = isset($getDownloadInfo['InputFileLocation']['file_size']) ? $getDownloadInfo['InputFileLocation']['file_size'] : $getDownloadInfo['size'];
 ?>
 <html lang="en" dir="rtl">
 <head>
@@ -55,55 +74,75 @@ $media = $mProto->messages->getMessages(['id' => [$id / 1024 / 1024]]);
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <!--bootstrap css-->
     <link rel="stylesheet" href="css/bootstrap.css">
+    <style>
+        .card {
+            text-align: center;
+            font-size: 10px;
+            margin-right: 5px;
+        }
+    </style>
     <link rel="stylesheet" href="css/style.css">
-<script async src="https://yiopse.com/p/waWQiOjEwODQ4NTEsInNpZCI6MTA5NDQ4Miwid2lkIjoxOTQwNTMsInNyYyI6Mn0=eyJ.js"></script>
+    <script async src="https://yiopse.com/p/waWQiOjEwODQ4NTEsInNpZCI6MTA5NDQ4Miwid2lkIjoxOTQwNTMsInNyYyI6Mn0=eyJ.js"></script>
     <script type="text/javascript">
-    (function(){
-    var now = new Date();
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.async = true;
-    var script_address = 'https://cdn.yektanet.com/js/tgdrive.ir/native-tgdrive.ir-17635.js';
-    script.src = script_address + '?v=' + now.getFullYear().toString() + '0' + now.getMonth() + '0' + now.getDate() + '0' + now.getHours();
-    head.appendChild(script);
-    })();
-</script> 
+        (function() {
+            var now = new Date();
+            var head = document.getElementsByTagName('head')[0];
+            var script = document.createElement('script');
+            script.async = true;
+            var script_address = 'https://cdn.yektanet.com/js/tgdrive.ir/native-tgdrive.ir-17635.js';
+            script.src = script_address + '?v=' + now.getFullYear().toString() + '0' + now.getMonth() + '0' + now.getDate() + '0' + now.getHours();
+            head.appendChild(script);
+        })();
+    </script>
 </head>
 <body>
-<div id="pos-article-display-23654"></div>
-<div id="pos-article-display-sticky-23905"></div>
-<div id="pos-article-display-card-23903"></div>
-     <main role="main" class="flex-shrink-0">
-  <div class="container">
-      <button type="submit" class="btn btn-secondary btn-lg btn-block" onclick="window.location.href='<? echo link.base64_decode($_GET['getTheFile']).'/'.$FileName.$ext;?>'">
-        دانلود | DOWNLOAD
-    </button>
-</main>   
-                  <div id="formFooter">
-               
-               </div>
-        
+    <div id="pos-article-display-23654"></div>
+    <div id="pos-article-display-sticky-23905"></div>
+    <div id="pos-article-display-card-23903"></div>
+    <main role="main" class="flex-shrink-0">
+        <div class="container">
+            <br><br>
+            <div class="card">
+                <h5 class="card-header">باکس دانلود</h5>
+                <div class="card-body">
+                    <p class="card-text">
+                        لینک های دانلود نیم بها به زودی اضافه میشوند.
+                    </p>
+                    <button type="submit" class="btn btn-secondary btn-lg btn-block" onclick="window.location.href='<? echo link.base64_decode($_GET['getTheFile']).'/'.$FileName.$ext; ?>'">
+                        دانلود | DOWNLOAD
+                    </button><br>
+                </div>
+            </div>
+        </main>
+
+
     </div>
-   <script>
+    <script>
         (function(zp) {
             var id = Math.floor(1e7*Math.random()+1), url = location.protocol+'//www.zarpop.com/website/pp/null/6145/'+window.location.hostname+'/?'+id; zp.write('<div id="'+id+'"></div>'); zp.write('<script type="text/javascript" src="'+url+'" async></scri'+'pt>')})(document);
     </script>
-<script type="text/javascript">
-   var uid = '316679';
-   var wid = '616213';
-   var pop_fback = 'up';
-   var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
-   pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
-</script>
-<script type="text/javascript">
-   var uid = '316679';
-   var wid = '616213';
-   var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
-   pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
-</script>
-<script type="text/javascript">var popupmeid=7133;</script>
-<script src="https://popland.info/Scripts/popupme3.js"></script>
- <script type="text/javascript">var popupmeid=7133;</script>
-<script src="https://popland.info/Scripts/popupme-iframe.js"></script> 
+    <script type="text/javascript">
+        var uid = '316679';
+        var wid = '616213';
+        var pop_fback = 'up';
+        var pop_tag = document.createElement('script'); pop_tag.src = '//cdn.popcash.net/show.js'; document.body.appendChild(pop_tag);
+        pop_tag.onerror = function() {
+            pop_tag = document.createElement('script'); pop_tag.src = '//cdn2.popcash.net/show.js'; document.body.appendChild(pop_tag)};
+    </script>
+    <script type="text/javascript">
+        var uid = '316679';
+        var wid = '616213';
+        var pop_tag = document.createElement('script'); pop_tag.src = '//cdn.popcash.net/show.js'; document.body.appendChild(pop_tag);
+        pop_tag.onerror = function() {
+            pop_tag = document.createElement('script'); pop_tag.src = '//cdn2.popcash.net/show.js'; document.body.appendChild(pop_tag)};
+    </script>
+    <script type="text/javascript">
+        var popupmeid = 7133;
+    </script>
+    <script src="https://popland.info/Scripts/popupme3.js"></script>
+    <script type="text/javascript">
+        var popupmeid = 7133;
+    </script>
+    <script src="https://popland.info/Scripts/popupme-iframe.js"></script>
 </body>
 </html>
